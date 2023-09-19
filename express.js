76ffd66a2f3aacb1832ba5e51a9a42789e98acb3
Miguel Herrera <49216976+mahsve@users.noreply.github.com>
2023-09-19 17:30:17 -0400

    Update .env

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('hbs');
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/components');
